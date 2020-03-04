7591ec1c97b834a32752ac40f85870b77eae5eec
rishi patel <rishipatel0702@gmail.com>
2020-03-03 21:16:38 -0500

    2 modified functions
    made changes to two functions for initializing and taking input polynomial variables.

--- poly.c
+++ poly.c
@@ -8,69 +8,64 @@
 
 #include "poly.h"
 
-
 /* 
   Initialize all coefficients and exponents of the polynomial to zero.
  */
-void init_polynom( int coeff[ ], int exp[ ] )
+void init_polynom(int coeff[], int exp[])
 {
 
-   /* ADD YOUR CODE HERE */
-   int i ; 
-   for(i = 0 ; i < )
+    /* ADD YOUR CODE HERE */
+    int i;
+    for (i = 0; i < ASIZE; i++)
+    {
+        coeff[i] = 0;
+        exp[i] = 0;
+    }
 
-}  /* end init_polynom */
-
+} /* end init_polynom */
 
 /*
   Get inputs from user using scanf() and store them in the polynomial.
  */
-void get_polynom( int coeff[ ], int exp[ ] )
+void get_polynom(int coeff[], int exp[])
 {
 
-   /* ADD YOUR CODE HERE */
+    /* ADD YOUR CODE HERE */
+    int i ; 
+    for(i = 0 ; i < ASIZE ; i++){
+        scanf("%d \n %d \n"coeff[i] , expo[i]) ; 
+    }
 
-}  /* end get_polynom */
-
+} /* end get_polynom */
 
 /*
   Convert the polynomial to a string s.
  */
-void polynom_to_string( int coeff[ ], int exp[ ], char s[ ] )
+void polynom_to_string(int coeff[], int exp[], char s[])
 {
 
-   /* ADD YOUR CODE HERE */
+    /* ADD YOUR CODE HERE */
 
-}  /* end polynom_to_string */
-
-
+} /* end polynom_to_string */
 
 /*
   Evaluate the polynomial for the value of x and store the result p(x) in variable result.
  */
-void eval_polynom( int coeff[ ], int exp[ ], double x, double *result )
+void eval_polynom(int coeff[], int exp[], double x, double *result)
 {
 
-   /* ADD YOUR CODE HERE */
+    /* ADD YOUR CODE HERE */
 
-}  /* end eval_polynom */
-
-
+} /* end eval_polynom */
 
 /*
   Add two polynomials and the result is stored in the first polynomial (arrays co1[] and ex1[]).
  */
-void add_polynom( int co1[ ], int ex1[ ], int co2[ ], int ex2[ ] )
+void add_polynom(int co1[], int ex1[], int co2[], int ex2[])
 {
 
-   /* ADD YOUR CODE HERE */
+    /* ADD YOUR CODE HERE */
 
-}  /* end add_ polynom */
-
+} /* end add_ polynom */
 
 /************************** END OF FILE ***************************/
-
-
-
-
-
