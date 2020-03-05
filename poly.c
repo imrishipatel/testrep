b96b039b0927cbb8365eceb54270abd671f4ef88
rishi patel <rishipatel0702@gmail.com>
2020-03-04 22:33:56 -0500

    major changes
    major changes in the code

--- poly.c
+++ poly.c
@@ -27,15 +27,18 @@ void init_polynom(int coeff[], int exp[])
 /*
   Get inputs from user using scanf() and store them in the polynomial.
  */
+int not;
+int size = 0; 
 void get_polynom(int coeff[], int exp[])
 {
 
     /* ADD YOUR CODE HERE */
-    int i, not;
+    int i;
     scanf("%d ", &not);
     for (i = 0; i < not; i++)
     {
         scanf("%d  %d ", coeff[i], exp[i]);
+        size++;
     }
 
 } /* end get_polynom */
@@ -47,12 +50,102 @@ void polynom_to_string(int coeff[], int exp[], char s[])
 {
 
     /* ADD YOUR CODE HERE */
+    int i, j = 0;
+    //size = sizeof(coeff);
+
+    int input;
+    int counter, k, d, a;
+    //char rev[] = 0 ;
+    //char temp[sizeof(coeff)] = 0;
+
+    for (i = 0; i < size; i++)
+    {
+
+        if (coeff[i] > 0)
+        {
+            /*
+            if (coeff[i] > 10 || coeff[i] == 10)
+            {
+
+                j = 10, counter = 0, d = 1;
+                input = coeff[i];
+                while (input != 0)
+                {
+                    k = input % j;
+                    a = k / d;
+                    temp[counter] = a + '0';
+                    input = input - k;
+                    //temp[counter + 1] = '\0';
+                    counter++;
+                    j *= 10;
+                    d *= 10;
+                }
+                int p, q;
+                for (p = 0, q = (sizeof(temp) - 2); q >= 0; q--, p++)
+                {
+                    rev[p] = temp[q];
+                    //s[p + 1] = '\0';
+                }
+            }*/
+
+            s[j++] = coeff[i] + '0';
+        }
+        else if (coeff[i] < 0)
+        {
+            s[j] = '-';
+
+            s[j + 1] = coeff[i] + '0';
+        }
+        else if (coeff[i] == 0)
+        {
+            s[j++] = coeff[i] + '0';
+        }
+        /*
+        else if (coeff[i] > 10 || coeff[i] == 10 || coeff[i] < -10 || coeff[i] == -10)
+        {
+            if (coeff[i] < -10 || coeff[i] == -10)
+            {
+                s[j] = '-';
+
+                j = 10, counter = 0, d = 1;
+                input = coeff[i];
+                while (input != 0)
+                {
+                    k = input % j;
+                    a = k / d;
+                    temp[counter] = a + '0';
+                    input = input - k;
+                    //temp[counter + 1] = '\0';
+                    counter++;
+                    j *= 10;
+                    d *= 10;
+                }
+                int p, q;
+                for (p = 0, q = (sizeof(temp) - 1); q >= 0; q--, p++)
+                {
+                    rev[p] = temp[q];
+                    rev[p + 1] = '\0';
+                    //s[p + 1] = '\0';
+                }
+                s[j + 1] = rev[j + 1];
+            }
+        }
+        */
+        s[j++] = 'x';
+        s[j++] = '^';
+        s[j++] = exp[i] + '0';
+    }
+    for(i = 0 ; i < size ; i++){
+        printf("%c" , s[i]) ; 
+    }
+    
+    /*
     int i;
     int input;
     int j, counter, k, d, a;
-    char temp[sizeof(coeff)] = 0 ; 
+    //char temp[sizeof(coeff)] = 0 ; 
 
-    for (i = 0; i < sizeof(coeff), i < sizeof(exp); i++)
+    for (i = 0; i < sizeof(coeff) ; i++)
     {
         if (coeff[i] < 10)
         {
@@ -75,14 +168,15 @@ void polynom_to_string(int coeff[], int exp[], char s[])
                  
             }
             int p,q; 
-            for(p = 0 , q = (sizeof(temp) - 1) ; q >=0  ; q-- , p++){
+            for(p = 0 , q = (sizeof(temp) - 2) ; q >=0  ; q-- , p++){
                 s[p] = temp[q] ; 
+                s[p+1] = '\0' ; 
             }
             
             
         }
     }
-
+    */
 } /* end polynom_to_string */
 
 /*
