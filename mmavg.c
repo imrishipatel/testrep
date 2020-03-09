217885c391c64fab9a75d0f5caa8620bc03daaa8
Rishi Patel <46664657+imrishipatel@users.noreply.github.com>
2020-03-09 14:08:22 -0400

    changed the strIntimplementation
    Removed pointer implementation from the code.

--- mmavg.c
+++ mmavg.c
@@ -11,13 +11,23 @@
 
 /*****  YOU MAY ADD YOUR OWN FUNCTION(S) HERE.  *****/
 
-int strInt(char *s)
+int strInt(char s[])
 {
   int i, n = 0;
 
-  for (i = 0; *(s + i) >= '0' && *(s + i) <= '9'; i++)
+  for (i = 1; s[i] >= '-' && s[i] <= '9'; i++)
   {
-    n = n * 10 + (*(s + i) - '0');
+
+    if (s[i] == '-')
+    {
+      n = n * 10 + (s[i + 1] - '0');
+      n = (-1) * n;
+      i++; 
+    }
+    else
+    {
+      n = n * 10 + (s[i] - '0');
+    }
   }
 
   return n;
@@ -37,34 +47,41 @@ main(int argc, char *argv[])
   /***** ADD YOUR CODE BELOW THIS LINE *****/
 
   int i, sum = 0;
+  
+  
+  max = strInt(argv[1]);
+  min = strInt(argv[1]);
 
-  max = stInt(*argv[0]);
-  min = stInt(*argv[0]);
-
-  for (i = 0; i < argc; i++)
+  if (argc > 0)
   {
-    if (*argv[i] >= '-' && *argv[i] <= '9')
+
+    for (i = 2; i <= (argc - 1) ; i++)
     {
-      if (*argv[i] == '-')
+      if (*argv[i] >= '-' && *argv[i] <= '9')
       {
-        i++;
+        sum += strInt(argv[i]);
       }
-      
-      sum += stInt(*argv[i]);
-    }
 
-    if (stInt(*argv[i]) > max)
-    {
-      max = stInt(*argv[i]);
-    }
+      if (strInt(argv[i]) > max)
+      {
+        max = strInt(argv[i]);
+      }
 
-    if (stInt(*argv[i]) < min)
-    {
-      min = stInt(*argv[i]);
+      if (strInt(argv[i]) < min)
+      {
+        min = strInt(argv[i]);
+      }
     }
+  }
+  else
+  {
+    printf("Usage: mmavg int1 int2 int3 ...\n");
+    exit(0);
   }
 
   avg = sum / (argc);
+
+  
 
   /***** ADD YOUR CODE ABOVE THIS LINE *****/
   /*****************************************/
