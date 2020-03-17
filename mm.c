1f2cfc080e1a13a624ac4555f272dbb2afc59a63
rishi patel <rishipatel0702@gmail.com>
2020-03-17 18:49:02 -0400

    assignment2
    installed assignment2 code for first question

---
+++ mm.c
@@ -0,0 +1,73 @@
+/***********************************
+* EECS 2031 - Assignment 2
+* Filename: mm.c
+* Author: Last name, first name
+* Email: Your preferred email address	 	
+* Login ID: Your EECS login ID	
+************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/***************** DO NOT USE ARRAY INDEXING *****************/
+/************** USE ONLY POINTERS IN THIS FILE ***************/
+
+/* You may define additional functions and local variables. */
+
+
+/* 
+   Save the three command-line arguments into nr1, nc1 and nc2.
+ */
+
+void get_args( char **argv, int *nr1, int *nc1, int *nc2 )
+{
+
+   /*************** ADD YOUR CODE HERE ***************/
+
+}
+
+
+
+
+/*
+   Initialize a 2-dimensional array.  
+   Element (i, j) is assigned value i+j.
+ */
+
+void initMatrix ( int **a, int nrows, int ncols )
+{
+
+   /*************** ADD YOUR CODE HERE ***************/
+
+}
+
+
+
+
+
+/* 
+   Multiply arrays a and b.  
+   Array a has nr1 rows and nc1 columns.
+   Array b has nc1 rows and nc2 columns.
+   Allocate an array c having nr1 rows and nc2 columns.
+   Compute a x b and store the result in array c.
+   Return array c.
+   If an error occurs (e.g., insufficient memory), return NULL pointer.
+ */
+
+
+int **matrix_mult( int **a, int **b, int nr1, int nc1, int nc2 )
+{
+
+   /*************** ADD YOUR CODE HERE ***************/
+
+   return( NULL );	/* replace this line with your code */
+}
+
+
+
+/************************* END OF FILE *************************/
+
+
